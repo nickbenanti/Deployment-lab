fd792f9a62a45ba1dbdc2793bc6f3483bb2b44c5
nick benanti <nickbenanti324@gmail.com>
2023-03-01 14:07:32 -0600

    deployment

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(`${__dirname}/public`))
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, () => console.log(`Server runnig on 4000`))
