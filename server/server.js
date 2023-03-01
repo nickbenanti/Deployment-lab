b7c22b1a4ca3d094fedbc60e9442ef37d0b8f57c
nick benanti <nickbenanti324@gmail.com>
2023-03-01 14:29:47 -0600

    fixxed errors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json())
+app.use(express.static(`public`))
 
 
+app.get('/',(req,res) =>{
+    res.status(200).sendFile(path.join(__dirname, '..public/index.html'))
+})
+app.get('/css',(req,res) =>{
+    res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
+})
 
-
-
-
+app.get('/js', (req,res) =>{
+    res.status(200).sendFile(path.join(__dirname, '../public/main.js'))
+})
 
 
 
