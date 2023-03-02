14c3395df554324b3e04465984ecd7850086c8ff
nick benanti <nickbenanti324@gmail.com>
2023-03-02 15:41:52 -0600

    added button

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 let bananas = document.querySelector('img')
+const yesBtn = document.getElementById('yesBtn')
+const noBtn = document.getElementById('noBtn')
+
+
+
 
 bananas.addEventListener('click', (evt) =>{
-    alert('Ya I bet you like those blue bananas wierdo')
+    alert("Nobody said anything about clicking on the bananas so why did you do that? Why don't you focus your attention on the question down there!")
 
 })
+
+const show = () =>{
+    document.getElementById('image').style.display = "block"
+    document.getElementById('yesBtn').style.display = "none"
+}
+
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
-const path = require('path')
 const app = express()
+const path = require('path')
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '7224845033234ae58e932631d151f9ec',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hey Nick you actually got it to work!')
 
 app.use(express.json())
 app.use(express.static(`public`))
+
+// const = {
+//     yesButton,
+
+// } = require('./controller')
 
 
 app.get('/',(req,res) =>{
@@ -15,7 +28,9 @@ app.get('/css',(req,res) =>{
 
 app.get('/js', (req,res) =>{
     res.status(200).sendFile(path.join(__dirname, '../public/main.js'))
+    rollbar.log('Used button')
 })
+
 
 
 
